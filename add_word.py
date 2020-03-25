3fdd9002faa34c3bc2a87edd93817bb298219e6d
jiahenghuang <huangjiaheng0901@163.com>
2020-03-25 18:59:33 +0800

    generate new word_to_pinyin

--- add_word.py
+++ add_word.py
@@ -1,18 +1,21 @@
-import pypinyin
+#coding:utf-8
+from pypinyin import pinyin, lazy_pinyin, Style
 
 def word2pinyin(word):
-	result = []
-	for char in word:
-		tmp = pypinyin.lazy_pinyin(char)[0].upper()+'_1'
-		result.append(tmp)
-	return ' '.join(result)
+    result = pinyin(word, style=Style.TONE3, heteronym=True)
+    result_new = []
+    for char in result:
+        char = char[0]
+        char = char[:-1].upper()+'_'+char[-1]   
+        result_new.append(char)
+    return ' '.join(result_new)
 
-with open('tmp3.txt') as fr:
+with open('word_to_pinyin.txt') as fr:
 	data=fr.readlines()
 
 with open('xxxx.txt','w') as fw:
 	for line in data:
-		line = line.strip()
+		line = line.strip().split('\t')[0]
 		result = word2pinyin(line)
 		if result:
 			fw.write(line+'\t'+result+'\n')
